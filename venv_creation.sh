32e6b3bd86262184ecc3c8892e57889f4ff9f54f
Christian Duffau-Rasmussen <cd@northbridgedata.com>
2018-09-18 08:39:40 +0200

    Added venv_creation script and added matplotlib to dev requirements.

---
+++ venv_creation.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+virtualenv --no-site-packages  -p /usr/bin/python3.5 venv
+venv/bin/pip3 install --upgrade pip
+venv/bin/pip3 install -r requirements.txt --process-dependency-links
