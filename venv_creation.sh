28d39ed677bcf0b276a526b81e8dfe6b197f0933
Christian Duffau-Rasmussen <cd@northbridgedata.com>
2019-09-15 22:55:43 +0200

    venv requiring python3 and not python3.5 explicitly.

--- venv_creation.sh
+++ venv_creation.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-virtualenv --no-site-packages  -p /usr/bin/python3.5 venv
+virtualenv --no-site-packages  -p /usr/bin/python3 venv
 venv/bin/pip3 install --upgrade pip
 venv/bin/pip3 install -r requirements.txt --process-dependency-links
